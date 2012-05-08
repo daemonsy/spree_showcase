227ea47b08c870fc4b8a36288efd15638b906495
Damon Aw <daemonsy@gmail.com>
2012-05-08 14:48:25 +0800

    Version 0.1
    With a default Awkward Showcase Generator

--- lib/generators/spree_showcase/install/install_generator.rb
+++ lib/generators/spree_showcase/install/install_generator.rb
@@ -1,15 +1,25 @@
 module SpreeShowcase
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../assets", __FILE__)
 
       def add_javascripts
+        puts %q{Copying JS assets into your app's javascripts/store directory. The file showcase.js is required by the extension and is used to activate slideshow.'}
+        copy_file "aw-showcase.js", "apps/assets/javascripts/store/showcase.js"
+
         append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_showcase\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_showcase\n"
+
       end
 
       def add_stylesheets
+        puts %q{showcase.css styles are required by the extension and can be changed in anyway to fit your own _showcase.html.erb}
+        copy_file "aw-showcase.css", "apps/assets/stylesheets/store/showcase.css"
         inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_showcase\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_showcase\n", :before => /\*\//, :verbose => true
+      end
+      
+      def add_partials
+        puts %q{This partial is the template structure for displaying the slideshow.}
+        copy_file "aw-slider.html.erb", "app/views/spree/slides/_showcase.html.erb"      
       end
 
       def add_migrations
@@ -23,6 +33,10 @@ module SpreeShowcase
          else
            puts 'Skiping rake db:migrate, don\'t forget to run it!'
          end
+      end
+      
+      def complete
+        puts %q{Everything is done! To summarize, _showcase.html.erb => HTML template, showcase.css & showcase.js => assets }
       end
     end
   end
