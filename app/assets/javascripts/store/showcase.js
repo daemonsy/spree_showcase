60f847eb22a86e3684a5be27a8e7b83ca0ee6a4f
Damon Aw <daemonsy@gmail.com>
2012-05-09 00:46:34 +0800

    Using Flex Slider for the Responsiveness

--- app/assets/javascripts/store/showcase.js
+++ app/assets/javascripts/store/showcase.js
@@ -9,7 +9,7 @@ $(window).load(function() {
 	slideshowSpeed: 12000,           //Integer: Set the speed of the slideshow cycling, in milliseconds
 	animationDuration: 500,         //Integer: Set the speed of animations, in milliseconds
 	directionNav: false,             //Boolean: Create navigation for previous/next navigation? (true/false)
-	controlNav: false,               //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
+	controlNav: true,               //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
 	keyboardNav: true,              //Boolean: Allow slider navigating via keyboard left/right keys
 	mousewheel: false,              //Boolean: Allow slider navigating via mousewheel
 	prevText: "Previous",           //String: Set the text for the "previous" directionNav item
@@ -22,8 +22,8 @@ $(window).load(function() {
 	animationLoop: true,            //Boolean: Should the animation loop? If false, directionNav will received "disable" classes at either end
 	pauseOnAction: true,            //Boolean: Pause the slideshow when interacting with control elements, highly recommended.
 	pauseOnHover: false,            //Boolean: Pause the slideshow when hovering over slider, then resume when no longer hovering
-	controlsContainer: "#showcase-nav",          //Selector: Declare which container the navigation elements should be appended too. Default container is the flexSlider element. Example use would be ".flexslider-container", "#container", etc. If the given element is not found, the default action will be taken.
-	manualControls: "",             //Selector: Declare custom control navigation. Example would be ".flex-control-nav li" or "#tabs-nav li img", etc. The number of elements in your controlNav should match the number of slides/tabs.
+	controlsContainer: ".flex-container",          //Selector: Declare which container the navigation elements should be appended too. Default container is the flexSlider element. Example use would be ".flexslider-container", "#container", etc. If the given element is not found, the default action will be taken.
+	manualControls: ".custom-controls li a",             //Selector: Declare custom control navigation. Example would be ".flex-control-nav li" or "#tabs-nav li img", etc. The number of elements in your controlNav should match the number of slides/tabs.
 	start: function(){},            //Callback: function(slider) - Fires when the slider loads the first slide
 	before: function(){},           //Callback: function(slider) - Fires asynchronously with each slider animation
 	after: function(){},            //Callback: function(slider) - Fires after each slider animation completes
