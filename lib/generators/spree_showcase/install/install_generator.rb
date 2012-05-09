851b9f1cb1be7bd5504a97c43ad4e59e0fad22df
Damon Aw <daemonsy@gmail.com>
2012-05-10 02:04:36 +0800

    Well, it should be usable now =)

--- lib/generators/spree_showcase/install/install_generator.rb
+++ lib/generators/spree_showcase/install/install_generator.rb
@@ -4,24 +4,44 @@ module SpreeShowcase
       source_root File.expand_path("../assets", __FILE__)
 
       def add_javascripts
-        puts %q{Copying JS assets into your app's javascripts/store directory. The file showcase.js is required by the extension and is used to activate slideshow.'}
-        copy_file "showcase.js", "app/assets/javascripts/store/showcase.js"
-
-        #append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_showcase\n"
-
+        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_showcase\n"
+        if copy_file "javascripts/store/showcase.js", "app/assets/javascripts/store/showcase.js"
+          puts "Copied showcase.js into app/assets/javascripts/store/showcase.js\n"
+        else
+          puts "[Failed] File copying showcase.js => app/assets/javascripts/store/showcase.js\n"
+        end
+        if copy_file "javascripts/store/jquery.flexslider.js", "app/assets/javascripts/store/jquery.flexslider.js"
+          puts "Copied jquery.flexslider.js into app/assets/javascripts/store/jquery.flexslider.js\n"
+        else
+          puts "[Failed] File copying jquery.flexslider.js => app/assets/javascripts/store/jquery.flexslider.js\n"
+        end
+        puts "Copying JS assets into your app's javascripts/store directory. The file showcase.js is required by the extension and is used to activate slideshow.\n"
       end
 
       def add_stylesheets
-        puts %q{showcase.css styles are required by the extension and can be changed in anyway to fit your own _showcase.html.erb}
-        copy_file "showcase.css.scss", "app/assets/stylesheets/store/showcase.css.scss"
-        #inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_showcase\n", :before => /\*\//, :verbose => true
+        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_showcase\n", :before => /\*\//, :verbose => true
+        if copy_file "stylesheets/store/showcase.css.scss", "app/assets/stylesheets/store/showcase.css.scss"
+          puts "Copied showcase.css.scss into app/assets/stylesheets/store/showcase.css.scss\n" 
+        else
+          puts "[Failed] File copying showcase.css.scss => app/assets/stylesheets/store/showcase.css.scss\n"
+        end
+        if copy_file "stylesheets/store/flexslider.css", "app/assets/stylesheets/store/flexslider.css"
+          puts "Copied flexslider into app/assets/stylesheets/store/flexslider.css. This is used for the default flexslider slider.\n" 
+        else
+          puts "[Failed] File copying flexslider.css => app/assets/stylesheets/store/flexslider.css\n"
+        end
+        puts "showcase.css styles are required by the extension and can be changed in anyway to fit your own _showcase.html.erb\n"
       end
       
 
       
       def add_partials
         puts %q{This partial is the template structure for displaying the slideshow.}
-        copy_file "_showcase.html.erb", "app/views/spree/slides/_showcase.html.erb"      
+        if copy_file "partials/_showcase.html.erb", "app/views/spree/slides/_showcase.html.erb"
+          puts "Copied Partial _showcase.html.erb into app/views/spree/slides/_showcase.html.erb\n"      
+        else
+          puts "[Failed] File copying _showcase.html.erb => app/views/spree/slides/_showcase.html.erb\n"
+        end
       end
 
       def add_migrations
@@ -38,7 +58,7 @@ module SpreeShowcase
       end
       
       def complete
-        puts %q{Everything is done! To summarize, _showcase.html.erb => HTML template, showcase.css & showcase.js => assets. Edit these 3 files to fit run the slideshow. }
+        puts "\nEverything is done! To summarize, _showcase.html.erb => HTML template, showcase.css.scss & showcase.js => assets. Edit these 3 files to fit run the slideshow."
       end
     end
   end
