5ebadae82fb44a9ea5c32797c8621f522d089e75
Damon Aw <daemonsy@gmail.com>
2013-01-05 17:20:56 +0000

    Refactor install generator's messages and copy JS file to vendor assets
    Instance variables used like this is probably no good, but I don't know how to create a method in the generator that accepts arguments.

--- lib/generators/spree_showcase/install/install_generator.rb
+++ lib/generators/spree_showcase/install/install_generator.rb
@@ -3,26 +3,56 @@ module SpreeShowcase
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../assets", __FILE__)
 
+      def messages
+        @messages||= []
+      end
+
+      def copy_message
+        status = status.to_s.capitalize
+        "Copy #{@source} into #{@dest}..."
+      end
+
       def add_javascripts
         append_file 'app/assets/javascripts/store/all.js', "//= require jquery.flexslider\n"
+        @source  = "vendor/javascripts/jquery.flexslider.js"
+        @dest    = "vendor/assets/javascripts/store/jquery.flexslider.js"
+        if copy_file(@source, @dest)
+          messages << copy_message
+        else
+          messages << copy_message
+        end
       end
 
       def add_stylesheets
         inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_showcase\n", :before => /\*\//, :verbose => true
-        if copy_file "stylesheets/store/showcase.css.scss", "app/assets/stylesheets/store/showcase.css.scss"
-          puts "Copied showcase.css.scss into app/assets/stylesheets/store/showcase.css.scss\n" 
+
+        @source = "stylesheets/store/showcase.css.scss"
+        @dest = "app/assets/stylesheets/store/showcase.css.scss"
+        if copy_file(@source, @dest)
+          messages << self.copy_message
         else
-          puts "[Failed] File copying showcase.css.scss => app/assets/stylesheets/store/showcase.css.scss\n"
+          messages << self.copy_message
+        end
+
+        @source = "vendor/stylesheets/jquery.flexslider.css"
+        @dest = "vendor/assets/stylesheets/store/jquery.flexslider.css"
+        if copy_file(@source, @dest)
+          messages << copy_message
+        else
+          messages << copy_message
         end
         puts "showcase.css styles are required by the extension and can be changed in anyway to fit your own _showcase.html.erb\n"
       end
 
       def add_partials
         puts %q{This partial is the template structure for displaying the slideshow and also includes the settings for the JavaScript plugin.}
-        if copy_file "partials/_showcase.html.erb", "app/views/spree/slides/_showcase.html.erb"
-          puts "Copied Partial _showcase.html.erb into app/views/spree/slides/_showcase.html.erb\n"      
+
+        @source = "partials/_showcase.html.erb"
+        @dest = "app/views/spree/slides/_showcase.html.erb"
+        if copy_file(@source, @dest)
+          messages << copy_message
         else
-          puts "[Failed] File copying _showcase.html.erb => app/views/spree/slides/_showcase.html.erb\n"
+          messages << copy_message
         end
       end
 
@@ -38,8 +68,11 @@ module SpreeShowcase
            puts 'Skiping rake db:migrate, don\'t forget to run it!'
          end
       end
-      
+
       def complete
+        puts "\n-------- Files copied --------\n"
+        puts messages.join("\n")
+        puts "\n---------------------------------\n"
         puts "\nEverything is done! To summarize, _showcase.html.erb => HTML template, showcase.css.scss & showcase.js.erb => assets. Edit these 3 files to fit run the slideshow."
         puts "\nPlease run rake assets:clean (IMPORTANT) to clear any precompiled assets from fresh install of Spree."
       end
