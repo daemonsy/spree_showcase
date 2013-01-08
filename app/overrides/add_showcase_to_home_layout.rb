14fcdef61b87ac44c0546fb880871a7d1f909489
Damon Aw <daemonsy@gmail.com>
2013-01-08 19:38:56 +0000

    Simplify rendering of showcase and let the user decide how to continue with the overrides

--- app/overrides/add_showcase_to_home_layout.rb
+++ app/overrides/add_showcase_to_home_layout.rb
@@ -1,8 +1,12 @@
+# This is an ugly hack.
+# I wanted to insert above the first element in home/index, hence meaning it only shows when home index is rendered.
+# I can't find a selector that says "select ONLY first element of partial", so I do a insert_before
+# So I used a current_page? helper in _showcase.html.erb
 
-  Deface::Override.new(
-    :virtual_path => 'spree/home/index',
-    :name => 'add_spree_slider_to_home_index',
-    :insert_before => "code[erb-silent]:contains('content_for :sidebar')",
-    :partial => "spree/slides/content_insert"
-  )
+Deface::Override.new(
+  :virtual_path => 'spree/layouts/spree_application',
+  :name => 'add_spree_slider_to_home_index',
+  :insert_top => "#wrapper",
+  :partial => "spree/slides/showcase.html.erb"
+)
 
